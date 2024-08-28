b4f478480c35128b41ba6796bc91256389e0178a
Ashir-Akram <muhammadashirakram@gmail.com>
2024-08-28 07:13:41 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,26 @@ import React from 'react'
 const page = () => {
 
   return (
-<div>Ashir Akram ali</div>
+<div><figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img className="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div>
+        Sarah Dayan
+      </div>
+      <div>
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure></div>
 
   )
 }
